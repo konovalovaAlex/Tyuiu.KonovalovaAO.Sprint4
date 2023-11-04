31b73d7a3611b85cdb6720ee82dcb37cf89e2555
konovalovaAlex <144355131+konovalovaAlex@users.noreply.github.com>
2023-11-04 18:45:50 +0500

    Task1

---
+++ Tyuiu.KonovalovaAO.Sprint4.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KonovalovaAO.Sprint4.Task1.V12.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint4.Task1.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 6, 6, 6, 7, 7, 7, 5, 5, 8, 8, 9, 9, 5 };
+            int wait = 3472875;
+            var result = ds.Calculate(array);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.KonovalovaAO.Sprint4.Task1.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonovalovaAO.Sprint4.Task1.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonovalovaAO.Sprint4.Task1.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("17deb465-1af5-4fb7-89bf-b2fa0b4669ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
