3c22fc89ee4ca9bd9149d6855acf15906a7ee2f5
konovalovaAlex <144355131+konovalovaAlex@users.noreply.github.com>
2023-11-04 20:24:55 +0500

    Task3

---
+++ Tyuiu.KonovalovaAO.Sprint4.Task3.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonovalovaAO.Sprint4.Task3.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonovalovaAO.Sprint4.Task3.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e77e9e50-047b-45e1-b3d1-4dc1623d9ff2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
