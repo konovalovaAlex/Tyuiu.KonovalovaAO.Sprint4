7fb849dbd21e796efa6765e5c5e8dba7c4a7b124
konovalovaAlex <144355131+konovalovaAlex@users.noreply.github.com>
2023-11-04 17:59:38 +0500

    Add project files.

---
+++ Tyuiu.KonovalovaAO.Sprint4.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KonovalovaAO.Sprint4.Task0.V5.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint4.Task0.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 4, 3, 5, 6, 7, 8, 9, 2 };
+            int res = ds.GetSumEvenArrEl(numsArray);
+            int a = 20;
+            Assert.AreEqual(20, res);
+        }
+    }
+}
---
+++ Tyuiu.KonovalovaAO.Sprint4.Task0.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonovalovaAO.Sprint4.Task0.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonovalovaAO.Sprint4.Task0.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d9adee5-98cd-43d7-a129-9c2dde37a68b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
