d885d8a119b348833e65d1d0243c9993dffe92f3
konovalovaAlex <144355131+konovalovaAlex@users.noreply.github.com>
2023-11-12 18:55:32 +0500

    Task6

---
+++ Tyuiu.KonovalovaAO.Sprint4.Task6.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonovalovaAO.Sprint4.Task6.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonovalovaAO.Sprint4.Task6.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("259e6af3-5d55-4170-8738-ee247b4c4264")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KonovalovaAO.Sprint4.Task6.V17/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KonovalovaAO.Sprint4.Task6.V17.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint4.Task6.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 | Выполнила Коновалова А. О. /СМАРТб-23-1 ";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #4                                                              *");
+            Console.WriteLine("* Тема: Класс Array                                                      *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #17                                                            *");
+            Console.WriteLine("* выполнила: Коновалова Александра Олеговна | СМАРТб-23-1                *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дан строковый массив данных [Python, JavaScript, Java, C#, Swift,      *");
+            Console.WriteLine("* Kotlin, Ruby] используя класс Array подсчитайте количество элементов,  *");
+            Console.WriteLine("* длина которых равна 6.                                                 *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            var codes = new string[] { "Python", "JavaScript", "Java", "C#", "Swift", "Kotlin", "Ruby" };
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i <= codes.Length - 1; i++)
+            {
+                Console.WriteLine(codes[i]);
+            }
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Количество элементов, длина которых равна 6:");
+            int nums = ds.Calculate(codes);
+            Console.WriteLine(nums);
+            Console.ReadKey();
+
+        }
+    }
+}
